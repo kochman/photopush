d5c3b7f7eff14ad3608d62f87ad68e824d289fce
Sidney Kochman <sidney@kochman.org>
2016-02-09 02:21:53 -0500

    Add status file

--- photopush.sh
+++ photopush.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+COMMIT="$(git rev-parse HEAD)"
+
 while true; do
 	if [ ! -d /mnt/photo/photopush ]; then
 		mount /mnt/photo
@@ -8,5 +10,15 @@ while true; do
 	fusermount -u ~/camera
 	gphotofs ~/camera
 	rsync -rtvhP --include=*.JPG --exclude=* /home/pi/camera/*/*/*/ /mnt/photo/photopush/photopush-pi
+
+	# create status file
+	echo "hostname: $(hostname)" > /tmp/photopush-status.txt
+	if find ~/camera -mindepth 1 -print -quit | grep -q .; then
+		echo "camera: connected" >> /tmp/photopush-status.txt
+	else
+		echo "camera: disconnected" >> /tmp/photopush-status.txt
+	fi
+	echo "commit: $COMMIT" >> /tmp/photopush-status.txt
+	mv /tmp/photopush-status.txt /mnt/photo/photopush/photopush-pi/status.txt
 	sleep 5
 done
